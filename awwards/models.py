27ff09c8f85c50a2bfaa3213f0aa2a43b9fbc13a
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-08 13:28:36 +0300

    create model class Ratings

--- awwards/models.py
+++ awwards/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 import datetime as dt
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 # Create your models here.
 
 class Profile(models.Model):
@@ -17,7 +15,7 @@ class Profile(models.Model):
         return f' {self.user.username} Profile'
 
 
-class Projects(models.Model):
+class Project(models.Model):
     title = models.CharField(max_length=155)
     photo = models.ImageField(manual_crop='1280x720')
     description = models.TextField(max_length=255)
@@ -31,3 +29,53 @@ class Projects(models.Model):
 
     def delete_post(self):
         self.delete()
+    
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def search_project(cls, title):
+        return cls.objects.filter(title__icontains=title).all()
+
+    @classmethod
+    def all_posts(cls):
+        return cls.objects.all()
+
+    def save_post(self):
+        self.save()
+
+
+RATE_CHOICES = [
+    (1, '1'),
+    (2, '2'),
+    (3, '3'),
+    (4, '4'),
+    (5, '5'),
+    (6, '6'),
+    (7, '7'),
+    (8, '8'),
+    (9, '9'),
+    (10, '10'),
+]
+class Rating(models.Model):
+    projects = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings', null=True)
+    design =  models.PositiveSmallIntegerField(choices=RATE_CHOICES, blank=True)
+    average_design = models.FloatField(default=0, blank=True)
+    usability =  models.PositiveSmallIntegerField(choices=RATE_CHOICES, blank=True)
+    average_usability = models.FloatField(default=0, blank=True)
+    content =  models.PositiveSmallIntegerField(choices=RATE_CHOICES, blank=True)
+    average_content = models.FloatField(default=0, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(project_id=id).all()
+        return ratings
+
+    def __str__(self):
+        return f'{self.post} Rating'
+
