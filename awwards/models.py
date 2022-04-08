251fbad5a505edacbbeaa26c4afeedba240f706f
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-08 12:38:10 +0300

    create model class Projects

--- awwards/models.py
+++ awwards/models.py
@@ -12,3 +12,22 @@ class Profile(models.Model):
     email = models.EmailField(blank=True, max_length=100)
     profile_pic = models.ImageField(upload_to='images/')
     bio = models.TextField(blank=True, max_length=1000)
+
+    def __str__(self):
+        return f' {self.user.username} Profile'
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=155)
+    photo = models.ImageField(manual_crop='1280x720')
+    description = models.TextField(max_length=255)
+    link = models.URLField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
+
+    def delete_post(self):
+        self.delete()
