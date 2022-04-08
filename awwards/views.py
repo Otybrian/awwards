649d9b8e134edb136fad929c4739f896a09d87ef
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-08 17:56:37 +0300

    add view function newProject

--- awwards/views.py
+++ awwards/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from awwards.email import send_welcome_email
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ProjectForm, UserProfileForm, RateForm 
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from .models import Profile, Project, Rating
+from django.contrib.auth import login, authenticate
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    photo = Project.objects.all().order_by('-id')
+    return render(request, 'home.html',{'photo':photo})
 
+@login_required(login_url='/accounts/login/')
+def newProject(request):
+    if request.method == 'POST':
+        form=ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form=ProjectForm()
+    return render(request, 'new_project.html', {'form': form})
