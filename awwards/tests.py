6dc711610817378b141f8973a49252fa52a7b4ec
Otybrian <brian.otieno@student.moringaschool.com>
2022-04-09 13:57:52 +0300

    write tests

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import  Profile,Project
 
 # Create your tests here.
+class ProjectTestClass(TestCase):
+  def setUp(self):
+        user = User.objects.create(username="brian")
+        self.project = Project(title="Title",image="image.url",description="Test Project description",link="https://Otybrian.github.io/Instagram/",user=user)
+
+  def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+  def test_save_project(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="brian")
+        self.profile = Profile(profile_pic="",bio="profile_pic",user=user,contact="email")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_profile(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_update_profile(self):
+        self.profile.update_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_profile(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
